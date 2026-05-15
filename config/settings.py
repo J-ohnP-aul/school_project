3b2ffd07dbd6c9359d17472aff163688a07d5aa5
J-ohnP-aul <johnpaulnoblemen@gmail.com>
2026-05-16 02:38:29 +0300

    hghh

--- config/settings.py
+++ config/settings.py
@@ -82,24 +82,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 if os.environ.get('RENDER'):
        DB_DIR = '/data'
 else:
        DB_DIR = BASE_DIR
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
